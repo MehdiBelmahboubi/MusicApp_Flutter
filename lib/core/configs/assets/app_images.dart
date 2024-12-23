73a2a5c5439d30a12f04d8699ccd36b42b4f34ba
MehdiBelmahboubi <belmehboubim@gmail.com>
2024-12-23 06:02:33 +0100

    modify app image class

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -3,4 +3,5 @@ class AppImages {
 
   static const String introImage = '${basePath}intro1.jpg';
   static const String choseModeImage = '${basePath}chose_mode.jpg';
+  static const String signupOrSignin = '${basePath}kanye.jpg';
 }
