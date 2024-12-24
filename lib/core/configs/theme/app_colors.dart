aff9e2f43beb4a8cc897dce16da637b1e29f3ffa
MehdiBelmahboubi <belmehboubim@gmail.com>
2024-12-24 01:44:17 +0100

    modify dark mode color

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 class AppColors{
   static const primary = Color(0xffF2F2F2);
   static const lightBackground = Color(0xffF2F2F2);
-  static const darkBackground = Color(0xff0D0C0C);
+  static const darkBackground = Color.fromARGB(255, 65, 195, 235);
   static const grey = Color(0xffBEBEBE);
   static const darkGrey = Color.fromARGB(255, 65, 195, 235);
 }
