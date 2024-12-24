94c02520e4c0e430ab504564ab64c612460838bd
MehdiBelmahboubi <belmehboubim@gmail.com>
2024-12-24 01:44:29 +0100

    add image to class

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -3,5 +3,5 @@ class AppImages {
 
   static const String introImage = '${basePath}intro1.jpg';
   static const String choseModeImage = '${basePath}chose_mode.jpg';
-  static const String signupOrSignin = '${basePath}kanye.jpg';
+  static const String signupOrSignin = '${basePath}bellie.png';
 }
