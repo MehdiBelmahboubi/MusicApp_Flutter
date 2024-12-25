067e3d2104f94f1cf819ed913cd4eb3fd11f1c41
MehdiBelmahboubi <belmehboubim@gmail.com>
2024-12-25 03:14:41 +0100

    add app vectors

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -5,4 +5,5 @@ class AppVectors {
   static const String logo = '${basePath}logoNotaZak$format';
   static const String moon = '${basePath}Moon$format';
   static const String sun = '${basePath}Sun$format';
+  static const String home_top_card = '${basePath}home$format';
 }
