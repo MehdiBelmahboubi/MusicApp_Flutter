3b4e505fe9034927b439a0c872df1a99424b7f40
MehdiBelmahboubi <belmehboubim@gmail.com>
2024-12-25 01:54:55 +0100

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,10 +6,28 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:music_app_flutter/core/configs/theme/app_theme.dart';
 import 'package:music_app_flutter/presentation/chose_mode/bloc/theme_cubit.dart';
 import 'package:music_app_flutter/presentation/splash/pages/splash.dart';
+import 'package:music_app_flutter/service_locator.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDh2G9jkBGYZsqCbVuN2SbsthTxNP2p7o8",
+            appId: "1:577440510376:android:0ed96e73fa4e2e7f576d9d",
+            messagingSenderId: "577440510376",
+            projectId: "577440510376"
+        )
+    ); // Initialisation Firebase
+
+    print("Firebase initialisé avec succès !");
+  } catch (e) {
+
+    print("Erreur${e}");
+  }
+  await Firebase.initializeApp();
+  await initializeDependencies();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
